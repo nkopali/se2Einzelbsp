818dc5e5ce7cf9486a2b521d48a7555063d21a70
nilsk <nils.kopali20@live.com>
2020-03-12 12:20:03 +0100

    Added Matrikel check function

--- app/src/main/java/com/example/se2bsp/MainActivity.java
+++ app/src/main/java/com/example/se2bsp/MainActivity.java
@@ -3,12 +3,55 @@ package com.example.se2bsp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
+
+    public TextView response;
+    public EditText sendmsg;
+    String serverMsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sendmsg = findViewById(R.id.message);
+        response = findViewById(R.id.serverResponse);
+    }
+
+    public void checkMat(View v) throws InterruptedException {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    Socket s = new Socket("se2-isys.aau.at", 53212);
+
+                    PrintWriter output = new PrintWriter(s.getOutputStream());
+
+                    output.println(sendmsg.getText().toString());
+                    output.flush();
+
+                    BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                    serverMsg = input.readLine();
+
+                    output.close();
+                    s.close();
+
+                } catch (Exception e ){
+                    response.setText(e.getMessage());
+                }
+            }
+        });
+        thread.start();
+        thread.join();
+        response.setText(serverMsg);
+
     }
 }
